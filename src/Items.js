48309158ae04fd3966b0d75a0b3cf1fc3cdbf554
Orison <orisontetteh@gmail.com>
2023-01-02 20:51:45 +0000

    new css

--- src/Items.js
+++ src/Items.js
@@ -2,32 +2,37 @@ import React from "react";
 
 function Items({ items }) {
   return (
-    <div className="grocery-list">
+    <div>
       {items.map((item) => {
         const { id, date, paymentName, payTo, amount, narration } = item;
         return (
-          <article className="grocery-Item" key={id}>
-            <section className="grocery-List">
-              <div className="grocery-Item">
-                <p className="item-title">Date</p>
-                <p>{date}</p>
-              </div>
-              <div className="grocery-Item">
-                <p>Payment Name</p>
-                <p>{paymentName}</p>
-              </div>
-              <div className="grocery-Item">
-                <p>Pay To</p>
-                <p>{payTo}</p>
-              </div>
-              <div className="grocery-Item">
-                <p>Amount</p>
-                <p>${amount}.00</p>
-              </div>
-              <p className="narration-Item">Your narration is indicated below</p>
-              <textarea rows="4" cols="3">{narration}</textarea>
-            </section>
-          </article>
+          <>
+            <h2 className="item-heading">Payment Track {id}</h2>
+            <article className="payment-Item" key={id}>
+              <section className="payment-List">
+                <div className="payment-Item">
+                  <p className="item-title">Date</p>
+                  <p>{date}</p>
+                </div>
+                <div className="payment-Item">
+                  <p className="item-title">Payment Name</p>
+                  <p>{paymentName}</p>
+                </div>
+                <div className="payment-Item">
+                  <p className="item-title">Pay To</p>
+                  <p>{payTo}</p>
+                </div>
+                <div className="payment-Item">
+                  <p className="item-title">Amount</p>
+                  <p>${amount}.00</p>
+                </div>
+                <p className="narration-title">Narration is indicated below</p>
+                <textarea rows="6" cols="3" className="narration-textarea">
+                  {narration}
+                </textarea>
+              </section>
+            </article>
+          </>
         );
       })}
     </div>
